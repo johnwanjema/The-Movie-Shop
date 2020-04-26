0bdbd6c29c1d2f8fd16766f6000ef5f97dd624e9
john <jonwanjema@gmail.com>
2020-04-26 12:34:22 +0300

    added state for popular movies

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,34 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import appService from '../services/app.service.js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {},
+  state: {
+    popularMovies: []
+  },
+  getters: {
+    popularMovies: state => state.popularMovies,
+  },
+  mutations: {
+    setPopularMovies: (state, popularMovies) => {
+      state.popularMovies = popularMovies
+    },
+  },
+  actions: {
+    getPopularMovies({ commit }, credentials) {
+      return new Promise(resolve => {
+        appService.getPopularMovies(credentials)
+          .then(data => {
+            commit('setPopularMovies',data)
+            resolve()
+          })
+          .catch(() => {
+
+          })
+      })
+    }
+  },
 });
