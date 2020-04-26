c1d690faa365fbff809c1199c19cbd9f000a7273
john <jonwanjema@gmail.com>
2020-04-26 19:11:30 +0300

    changed layout of app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
+<style scoped>
+.nav-link{
+    color: blue;
+}
+</style>
 <template>
     <section>
         <div id="nav">
             <div>
-                <b-navbar toggleable="lg" type="dark" variant="info">
-                    <b-navbar-brand href="#">Movies Database</b-navbar-brand>
+                <b-navbar toggleable="lg" type="dark" variant="dark">
+                    <b-navbar-brand href="/">Movies Database</b-navbar-brand>
 
                     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-                    <b-collapse id="nav-collapse" is-nav>
-                        <b-navbar-nav>
-                            <b-nav-item href="#">Link</b-nav-item>
-                            <b-nav-item href="#">Disabled</b-nav-item>
+                    <b-collapse id="nav-collapse"  is-nav>
+                        <b-navbar-nav style="color:white" >
+                            <b-nav-item  href="#">Popular</b-nav-item>
+                            <b-nav-item  href="#">Trending</b-nav-item>
                         </b-navbar-nav>
 
                         <!-- Right aligned nav items -->
