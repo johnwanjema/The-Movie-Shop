626246b821f831df8b6500e4704c6aa40d1de81a
john <jonwanjema@gmail.com>
2020-04-26 11:04:53 +0300

    added bootsrap vue

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 new Vue({
   router,
   store,
