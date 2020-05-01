c39e2bb787b0f8803e498da7fcd4cab3c09b3f30
john <jonwanjema@gmail.com>
2020-05-01 23:04:52 +0300

    added global navbar and search

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,61 @@
 <style scoped>
 .nav-link{
-    color: blue;
+    color:white !important
 }
 </style>
+</style>
+
 <template>
     <section>
-        <div id="nav">
-            <div>
-                <b-navbar toggleable="lg" type="dark" variant="dark">
-                    <b-navbar-brand href="/">Movies Database</b-navbar-brand>
-
-                    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-                    <b-collapse id="nav-collapse"  is-nav>
-                        <b-navbar-nav style="color:white" >
-                            <b-nav-item  href="#">Popular</b-nav-item>
-                            <b-nav-item  href="#">Trending</b-nav-item>
-                        </b-navbar-nav>
-
-                        <!-- Right aligned nav items -->
-                        <b-navbar-nav class="ml-auto">
-                            <b-nav-form>
-                                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                            </b-nav-form>
-
-                            <b-nav-item-dropdown right>
-                                <!-- Using 'button-content' slot -->
-                                <template v-slot:button-content>
-                                    <em>User</em>
-                                </template>
-                                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                            </b-nav-item-dropdown>
-                        </b-navbar-nav>
-                    </b-collapse>
-                </b-navbar>
-            </div>
-        </div>
-        <router-view />
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand href="/">The Movie Shop</b-navbar-brand>
+    
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav style="color:white">
+                    <b-nav-item to="/popular">Popular</b-nav-item>
+                    <b-nav-item href="/trending">Trending</b-nav-item>
+                </b-navbar-nav>
+    
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                        <b-form-input size="sm" v-model="search" class="mr-sm-2" placeholder="Search"></b-form-input>
+                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                    </b-nav-form>
+    
+                    <b-nav-item-dropdown right>
+                        <!-- Using 'button-content' slot -->
+                        <template v-slot:button-content>
+                        <em>User</em>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+        </b-navbar-nav>
+    </b-collapse>
+</b-navbar>
+               <router-view />
     </section>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            search:'',
+            searching:true
+        }
+    },
+    watch:{
+        search() {
+            this.searchMovie();
+        }
+    },
+    methods:{
+        searchMovie(){
+            console.log('qwqw')
+        }
+    }
+}
+</script>
