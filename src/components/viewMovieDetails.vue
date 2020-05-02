c0db6faf183092c7b3fa900c98ad22f6feb3fcd9
john <jonwanjema@gmail.com>
2020-05-02 19:41:50 +0300

    added navbar to view movie details

--- src/components/viewMovieDetails.vue
+++ src/components/viewMovieDetails.vue
@@ -7,10 +7,36 @@
     height: 500px;
     width: 500px;
 }
+.nav-link{
+    color:white !important
+}
 </style>
 
 <template>
     <section>
+        <b-navbar toggleable="lg" fixed="top" type="dark" variant="dark">
+            <b-navbar-brand href="/">The Movie Shop</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav style="color:white">
+                    <b-nav-item to="/popular">Popular</b-nav-item>
+                    <b-nav-item to="/trending">Trending</b-nav-item>
+                    <b-nav-item to="/popularShows">Popular Shows</b-nav-item>
+                </b-navbar-nav>
+    
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item-dropdown right>
+                        <!-- Using 'button-content' slot -->
+                        <template v-slot:button-content>
+                            <em>User</em>
+                        </template>
+                    <b-dropdown-item href="#">Profile</b-dropdown-item>
+                    <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-6">
@@ -48,8 +74,8 @@ export default {
     methods: {
         getMoviewDetails() {
             axios
-                .get('https://api.themoviedb.org/3/movie/'+ this.movie.id +'?api_key=f16a94a392138ff37753fb6821113944&language=en-US')
-                .then(({data}) => {
+                .get('https://api.themoviedb.org/3/movie/' + this.movie.id + '?api_key=f16a94a392138ff37753fb6821113944&language=en-US')
+                .then(({ data }) => {
                     // console.log(data)
                     this.movieDetails = data
                 })
